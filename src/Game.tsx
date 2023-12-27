ff042b0652513daa39081c0fd0763eec1922dec7
John Serrano <andrey_201015@hotmail.com>
2023-12-27 16:18:49 -0500

    Add method selectAnswer

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography } from '@mui/material'
+import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
@@ -6,8 +6,14 @@ import { useQuestionsStore } from './store/questions'
 import { type Question as QuestionType } from './types'
 
 const Question = ({ data }: { data: QuestionType }) => {
+  const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+
+  const handleAnswerClick = (answerIndex: number) => () => {
+    selectAnswer(data.id, answerIndex)
+  }
+
   return (
-    <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left' }}>
+    <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left', marginTop: 5 }}>
       <Typography variant='h5'>
         {data.question}
       </Typography>
@@ -15,6 +21,16 @@ const Question = ({ data }: { data: QuestionType }) => {
       <SyntaxHighlighter language='javascript' style={atomOneDark}>
         {data.code}
       </SyntaxHighlighter>
+
+      <List sx={{ bgcolor: '#333' }} disablePadding>
+        {data.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton onClick={handleAnswerClick(index)}>
+              <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   )
 }
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -5,9 +5,10 @@ interface State {
   questions: Question[]
   currentQuestion: number
   getQuestions: (limit: number) => Promise<void>
+  selectAnswer: (questionId: number, answerIndex: number) => void
 }
 
-export const useQuestionsStore = create<State>()((set) => ({
+export const useQuestionsStore = create<State>()((set, get) => ({
   questions: [], // initial state questions
   currentQuestion: 0,
   getQuestions: async (limit) => {
@@ -16,5 +17,22 @@ export const useQuestionsStore = create<State>()((set) => ({
 
     const questions = data.sort(() => Math.random() - 0.5).slice(0, limit)
     set({ questions }) // question is ⬆ initial state
+  },
+  selectAnswer: (questionId, answerIndex) => {
+    const { questions } = get()
+
+    const newQuestions = structuredClone(questions) // clonar
+    const questionIndex = newQuestions.findIndex(q => q.id === questionId)
+    const questionData = newQuestions[questionIndex]
+
+    const isCorrectUserAnswer = questionData.correctAnswer === answerIndex
+
+    newQuestions[questionIndex] = {
+      ...questionData,
+      userSelectedAnswer: answerIndex,
+      isCorrectUserAnswer
+    }
+
+    set({ questions: newQuestions })
   }
 }))
