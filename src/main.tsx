a2bf6627dff3598fe3238a626a482ac9e1d41309
John Serrano <andrey_201015@hotmail.com>
2023-12-27 14:43:11 -0500

    Add dark mode material mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.tsx'
 import './index.css'
 
@@ -8,8 +10,17 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
