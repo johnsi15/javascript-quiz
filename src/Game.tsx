abd38d3926bd8c3bb4cbbc5b8e75059fd1a75433
John Serrano <andrey_201015@hotmail.com>
2023-12-27 12:25:12 -0500

    Add package syntaxHighlinghter and style atomOneDark

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,13 +1,20 @@
 import { Card, Typography } from '@mui/material'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+
 import { useQuestionsStore } from './store/questions'
 import { type Question as QuestionType } from './types'
 
 const Question = ({ data }: { data: QuestionType }) => {
   return (
-    <Card variant='outlined'>
+    <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left' }}>
       <Typography variant='h5'>
         {data.question}
       </Typography>
+
+      <SyntaxHighlighter language='javascript' style={atomOneDark}>
+        {data.code}
+      </SyntaxHighlighter>
     </Card>
   )
 }
