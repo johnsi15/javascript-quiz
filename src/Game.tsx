35d17ff2afe0bc096cfd17bdb260fd28d7412562
John Serrano <andrey_201015@hotmail.com>
2023-12-28 16:08:49 -0500

    Add component Question

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,59 +1,10 @@
-import { Card, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Typography } from '@mui/material'
+import { IconButton, Stack } from '@mui/material'
 import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-import confetti from 'canvas-confetti'
 import { Footer } from './Footer'
 
 import { useQuestionsStore } from './store/questions'
-import { type Question as QuestionType } from './types'
 
-const getBackgroundColor = (data: QuestionType, index: number) => {
-  const { userSelectedAnswer, correctAnswer } = data
-
-  if (userSelectedAnswer == null) return 'transparent'
-
-  if (index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
-
-  if (index === correctAnswer) return '#06D6A0'
-  if (index === userSelectedAnswer) return '#EF476F'
-}
-
-const Question = ({ data }: { data: QuestionType }) => {
-  const selectAnswer = useQuestionsStore(state => state.selectAnswer)
-
-  const handleAnswerClick = (answerIndex: number) => () => {
-    selectAnswer(data.id, answerIndex)
-
-    if (data.correctAnswer === answerIndex) confetti()
-  }
-
-  return (
-    <Card variant='outlined' sx={{ bgcolor: '#151515', p: 2, textAlign: 'left' }}>
-      <Typography variant='h5'>
-        {data.question}
-      </Typography>
-
-      <SyntaxHighlighter language='javascript' style={atomOneDark}>
-        {data.code}
-      </SyntaxHighlighter>
-
-      <List sx={{ bgcolor: '#1a212f' }} disablePadding>
-        {data.answers.map((answer, index) => (
-          <ListItem key={index} disablePadding divider>
-            <ListItemButton
-              disabled={data.userSelectedAnswer != null}
-              onClick={handleAnswerClick(index)}
-              sx={{ bgcolor: getBackgroundColor(data, index) }}
-            >
-              <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Card>
-  )
-}
+import { Question } from './Question'
 
 export function Game () {
   const questions = useQuestionsStore(state => state.questions)
---
+++ src/Question.tsx
@@ -0,0 +1,54 @@
+import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import confetti from 'canvas-confetti'
+
+import { useQuestionsStore } from './store/questions'
+import { type Question as QuestionType } from './types'
+
+const getBackgroundColor = (data: QuestionType, index: number) => {
+  const { userSelectedAnswer, correctAnswer } = data
+
+  if (userSelectedAnswer == null) return 'transparent'
+
+  if (index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+
+  if (index === correctAnswer) return '#06D6A0'
+  if (index === userSelectedAnswer) return '#EF476F'
+}
+
+export function Question ({ data }: { data: QuestionType }) {
+  const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+
+  const handleAnswerClick = (answerIndex: number) => () => {
+    selectAnswer(data.id, answerIndex)
+
+    if (data.correctAnswer === answerIndex) confetti()
+  }
+
+  return (
+    <Card variant='outlined' sx={{ bgcolor: '#151515', p: 2, textAlign: 'left' }}>
+      <Typography variant='h5'>
+        {data.question}
+      </Typography>
+
+      <SyntaxHighlighter language='javascript' style={atomOneDark}>
+        {data.code}
+      </SyntaxHighlighter>
+
+      <List sx={{ bgcolor: '#1a212f' }} disablePadding>
+        {data.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton
+              disabled={data.userSelectedAnswer != null}
+              onClick={handleAnswerClick(index)}
+              sx={{ bgcolor: getBackgroundColor(data, index) }}
+            >
+              <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Card>
+  )
+}
